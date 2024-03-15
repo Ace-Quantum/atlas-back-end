8b77d0dfbaf6c4756da4eda7b2f302dff9372d7d
root <root@DESKTOP-FADRK94>
2024-03-15 14:15:33 -0500

    Ok let's hope that's it. I've spent a lot of time testing locally

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,9 +4,6 @@
 I'm documenting for the check
 """
 
-if __name__ == "__main__":
-    retrieve_to_do
-
 import json
 import requests
 import sys
@@ -17,17 +14,34 @@ def retrieve_to_do(emp_id):
     """
     More documentation for the check
     """
+    emp_to_do_all = {}
+    emp_to_do_done = []
+
     site_url = "https://jsonplaceholder.typicode.com/"
-    emp_url = format("{site_url}/users/{emp_id}")
-    to_do_url = format("{site_url}/todos")
+    emp_url = f"{site_url}users/{emp_id}"
+    to_do_url = f"{emp_url}/todos"
 
-    employee_data = requests.get(format("{emp_url}"))
-    to_dos_data = requests.get(format("{to_do_url}"))
+    # print({emp_url})
 
-    if employee_data.status_code == 200 or to_dos_data.status_code == 200:
-        employee = employee_data.json()
-        to_dos = to_dos_data()
+    employee_data = requests.get(emp_url)
+    to_dos_data = requests.get(to_do_url)
 
-        print("{employee}")
-    else:
-        print(f"nope")
+    emp_dict = employee_data.json()
+    emp_to_do_all = to_dos_data.json()
+
+    emp_name = emp_dict.get("name")
+
+    for task in emp_to_do_all:
+        if task.get("completed") is True:
+            # print(task)
+            emp_to_do_done.append(task)
+
+    print(f"Employee {emp_name} is done with tasks({len(emp_to_do_done)}/{len(emp_to_do_all)}):")
+
+    for task in emp_to_do_done:
+        print("  " + task.get("title"))
+
+
+
+if __name__ == "__main__":
+    retrieve_to_do(sys.argv[1])
