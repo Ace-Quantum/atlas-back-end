56fb438665ddf440f6ee617aa0180728544fdf5d
root <root@DESKTOP-FADRK94>
2024-03-15 14:53:42 -0500

    idk if I should test this locally

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+"""
+I'm documenting for the check
+"""
+
+import csv
+import json
+import requests
+import sys
+import urllib
+
+
+def retrieve_to_do(emp_id):
+    """
+    More documentation for the check
+    """
+    todo_all = {}
+    todo_done = []
+
+    fields = ['USER_ID', 'USERNAME', 'STATUS', 'TITLE']
+    filename = f"{emp_id}.csv"
+
+    site_url = "https://jsonplaceholder.typicode.com/"
+    emp_url = f"{site_url}users/{emp_id}"
+    to_do_url = f"{emp_url}/todos"
+
+    # print({emp_url})
+
+    employee_data = requests.get(emp_url)
+    to_dos_data = requests.get(to_do_url)
+
+    emp_dict = employee_data.json()
+    todo_all = to_dos_data.json()
+
+    emp_name = emp_dict.get("name")
+
+    for task in todo_all:
+        if task.get("completed") is True:
+            # print(task)
+            todo_done.append(task)
+
+    print(f"Employee {emp_name} is done with tasks", end="")
+    print(f"({len(todo_done)}/{len(todo_all)}):")
+
+    for task in todo_done:
+        print("\t " + task.get("title"))
+
+    with open(filename, 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fields)
+
+        writer.writeheader()
+
+        writer.writerows(todo_all)
+
+
+if __name__ == "__main__":
+    retrieve_to_do(sys.argv[1])
