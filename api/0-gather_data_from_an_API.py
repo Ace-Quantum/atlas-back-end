567ff17f056021b4b0ed80a8f624e6c7c5315044
root <root@DESKTOP-FADRK94>
2024-03-15 14:21:20 -0500

    how long is too long

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -14,8 +14,8 @@ def retrieve_to_do(emp_id):
     """
     More documentation for the check
     """
-    emp_to_do_all = {}
-    emp_to_do_done = []
+    emp_todo_all = {}
+    emp_todo_done = []
 
     site_url = "https://jsonplaceholder.typicode.com/"
     emp_url = f"{site_url}users/{emp_id}"
@@ -27,19 +27,18 @@ def retrieve_to_do(emp_id):
     to_dos_data = requests.get(to_do_url)
 
     emp_dict = employee_data.json()
-    emp_to_do_all = to_dos_data.json()
+    emp_todo_all = to_dos_data.json()
 
     emp_name = emp_dict.get("name")
 
-    for task in emp_to_do_all:
+    for task in emp_todo_all:
         if task.get("completed") is True:
             # print(task)
-            emp_to_do_done.append(task)
+            emp_todo_done.append(task)
 
-    print(f"Employee {emp_name} is done with tasks({len(
-        emp_to_do_done)}/{len(emp_to_do_all)}):")
+    print(f"Employee {emp_name} is done with tasks({len(emp_todo_done)}/{len(emp_todo_all)}):")
 
-    for task in emp_to_do_done:
+    for task in emp_todo_done:
         print("  " + task.get("title"))
 
 
